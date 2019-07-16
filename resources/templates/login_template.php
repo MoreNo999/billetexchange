513d0aa47363dabf6b32515a3baf6d6b84e323ce
joshua pontes <josh.pontes99@gmail.com>
2019-07-16 07:28:37 -0700

    file structure adjustment

---
+++ resources/templates/login_template.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Billet Trading Login</title>
+            <link rel="stylesheet" type="text/css" href="./css/style.css">
+        <body>
+            <div class="loginbox">
+                <h1>Login Here</h1>
+                <form action="./scripts/login.php" method="POST">
+                    <p>Username</p>
+                    <input type="text" id="user" name="user" placeholder="Enter Username">
+                    <p>Password</p>
+                    <input type="password" id="pass" name="pass" placeholder="Enter Password"><br>
+                    <input type="submit" id="submit" value="login">
+                    <a href="#">Forgot Password?</a>
+                    <img src="./img/content/USAF_logo.png" width="300" height="200">
+                    
+                </form>
+            </div>
+
+        </body>
+    
+    </head>
+</html>

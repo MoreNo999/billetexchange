35fd6f715a3808210c1bec079ce4f332d31a2b23
zach <zacherycorn@gmail.com>
2019-07-16 11:45:58 -0500

    New user test page

---
+++ new_user.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Billet Trading New User</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+        <body>
+            <div class="Account Creation Box">
+                <h1>Create your account</h1>
+                <form action="./scripts/login.php" method="POST">
+                    <p>Username</p>
+                    <input type="text" id="user" name="user" placeholder="Enter Username ">
+                    <p>Password</p>
+                    <input type="password" id="pass" name="pass" placeholder="Enter Password">
+                    <p>Email Address</p>
+                    <input type="text" id="user" name="user" placeholder="Enter Email address">
+                    <p>Unit</p>
+                    <input type="password" id="pass" name="pass" placeholder="Enter Unit">
+                    <p>Phone number</p>
+                    <input type="password" id="pass" name="pass" placeholder="Enter Phone number (optional)">
+                    <p>Rank</p>
+                    <input type="password" id="pass" name="pass" placeholder="Enter Rank"><br>
+                    <input type="submit" id="submit" value="Create User">
+                    <img src="./img/content/USAF_logo.png" width="300" height="200">
+                    
+                </form>
+            </div>
+
+        </body>
+    </head>
+</html>

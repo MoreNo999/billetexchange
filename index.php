d1e110604c6557a05db866c96c8bd90b9df66e91
zach <zacherycorn@gmail.com>
2019-07-16 13:39:02 -0500

    made fields requried

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
                 <h1>Login Here</h1>
                 <form action="./scripts/login.php" method="POST">
                     <p>Username</p>
-                    <input type="text" id="user" name="user" placeholder="Enter Username">
+                    <input type="text" id="user" name="user" required placeholder="Enter Username">
                     <p>Password</p>
-                    <input type="password" id="pass" name="pass" placeholder="Enter Password"><br>
+                    <input type="password" id="pass" name="pass" required placeholder="Enter Password"><br>
                     <input type="submit" id="submit" value="login">
                     <a href="#">Forgot Password?</a>
                     <a href="./new_user.php" class="button">Create Account</a>                 
--- new_user.php
+++ new_user.php
@@ -8,13 +8,13 @@
                 <h1>Create your account</h1>
                 <form action="./scripts/CreateUser.php" method="POST">
                     <p>Username</p>
-                    <input type="text" id="user" name="user" placeholder="Enter Username ">
+                    <input type="text" id="user" name="user" required placeholder="Enter Username ">
                     <p>Password</p>
-                    <input type="password" id="pass" name="pass" placeholder="Enter Password">
+                    <input type="password" id="pass" name="pass" required placeholder="Enter Password">
                     <p>Email Address</p>
-                    <input type="text" id="email" name="email" placeholder="Enter .mil Email Address">
+                    <input type="text" id="email" name="email" required placeholder="Enter .mil Email Address">
                     <p>Unit</p>
-                    <input type="text" id="unit" name="unit" placeholder="Enter Unit">
+                    <input type="text" id="unit" name="unit" required placeholder="Enter Unit">
                     <p>Phone number</p>
                     <input type="text" id="phone" name="phone" placeholder="Enter Phone Number (optional)">
                     <input type="submit" id="submit" value="Create User">

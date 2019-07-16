b0fed4510fb7da82fa0823e4a8ae869906ee24b7
Tim Carlisle <gwa2100@gmail.com>
2019-07-15 21:11:03 -0500

    config.php added

---
+++ config.php
@@ -0,0 +1,57 @@
+<?php
+ 
+/*
+    This is the main configuration file.  This connects the other .php files to the database, as well as defines paths.
+    Include in all php files that require the settings.
+*/
+ 
+$config = array(
+    "db" => array(
+        "db1" => array(
+            "dbname" => "database1",
+            "username" => "dbUser",
+            "password" => "pa$$",
+            "host" => "localhost"
+        ),
+        "db2" => array(
+            "dbname" => "database2",
+            "username" => "dbUser",
+            "password" => "pa$$",
+            "host" => "localhost"
+        )
+    ),
+    "urls" => array(
+        "baseUrl" => "http://billetfinder.adamantfoundation.org"
+    ),
+    "paths" => array(
+        "resources" => "/path/to/resources",
+        "images" => array(
+            "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
+            "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+        )
+    )
+);
+ 
+/*
+    I will usually place the following in a bootstrap file or some type of environment
+    setup file (code that is run at the start of every page request), but they work 
+    just as well in your config file if it's in php (some alternatives to php are xml or ini files).
+*/
+ 
+/*
+    Creating constants for heavily used paths makes things a lot easier.
+    ex. require_once(LIBRARY_PATH . "Paginator.php")
+*/
+defined("LIBRARY_PATH")
+    or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+     
+defined("TEMPLATES_PATH")
+    or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+ 
+/*
+    Error reporting.
+*/
+ini_set("error_reporting", "true");
+error_reporting(E_ALL|E_STRCT);
+ 
+?>

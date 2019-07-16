9cd1439acd9735deeb960e057a407535a11e41bb
Tim Carlisle <gwa2100@gmail.com>
2019-07-16 13:03:54 -0500

    Secured authentication

--- home.php
+++ home.php
@@ -1,9 +1,14 @@
 <?php
+    session_start();
     require_once("scripts/functions.php");
     // load up your config file
     require_once("config.php");
      
     require_once(TEMPLATES_PATH . "/header.php");
+
+    if (!isset($_SESSION['id'])){
+        header('Location: index.php');
+    }
 ?>
 <div id="container">
     <div id="content">

ca9a3b5d2e20b79e8e59b90c6400afcea7f223ae
zach <zacherycorn@gmail.com>
2019-07-16 12:07:31 -0500

    Edited form, cleaned up stuff

--- new_user.php
+++ new_user.php
@@ -4,23 +4,20 @@
         <title>Billet Trading New User</title>
         <link rel="stylesheet" type="text/css" href="./css/style.css">
         <body>
-            <div class="Account Creation Box">
+            <div class="loginbox">
                 <h1>Create your account</h1>
-                <form action="./scripts/login.php" method="POST">
+                <form action="./scripts/CreateUser.php" method="POST">
                     <p>Username</p>
                     <input type="text" id="user" name="user" placeholder="Enter Username ">
                     <p>Password</p>
                     <input type="password" id="pass" name="pass" placeholder="Enter Password">
                     <p>Email Address</p>
-                    <input type="text" id="user" name="user" placeholder="Enter Email address">
+                    <input type="text" id="user" name="user" placeholder="Enter .mil Email Address">
                     <p>Unit</p>
                     <input type="password" id="pass" name="pass" placeholder="Enter Unit">
                     <p>Phone number</p>
-                    <input type="password" id="pass" name="pass" placeholder="Enter Phone number (optional)">
-                    <p>Rank</p>
-                    <input type="password" id="pass" name="pass" placeholder="Enter Rank"><br>
+                    <input type="password" id="pass" name="pass" placeholder="Enter Phone Number (optional)">
                     <input type="submit" id="submit" value="Create User">
-                    <img src="./img/content/USAF_logo.png" width="300" height="200">
                     
                 </form>
             </div>

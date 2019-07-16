668a6d4f03601d3acfc5c734673f1e068a140a2c
joshua pontes <josh.pontes99@gmail.com>
2019-07-16 08:12:09 -0700

    added header.php

---
+++ resources/templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <title>Billet Trading</title>
+            <link rel="stylesheet" type="text/css" href="../../css/header.css">
+    </head>
+    
+    <body>
+            <div class="header">
+                    <h1>Billet Trading Home</h1>
+                    <table style="width:100%">
+                            <tr>
+                              <th><a href="#">Home</a></th>
+                              <th><a href="#">Browse</a></th>
+                              <th><a href="#">Post</a></th>
+                              <th><a href="#">Matches</a></th>
+                              <th><a href="#">My Account</a></th>
+                            </tr>
+                    </table>
+            </div>              
+    </body>
+
+</html>

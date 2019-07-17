6c8c365701d7a85571da2b3f6844c8ef5f906664
Moreno <jansen.moreno999@gmail.com>
2019-07-17 15:26:16 -0500

    css

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 </head>
-<link rel="stylesheet" type="text/css" href="./css/footer.css?ver=5">
+<link rel="stylesheet" type="text/css" href="./css/footer.css?ver=6">
 <body>
 <div class="footer">
 	<table>

dc1b5d5e73f9705e8d4f99a9872a2756a5a314db
Tim Carlisle <gwa2100@gmail.com>
2019-07-17 10:43:47 -0500

    Changed form fields to standardized names
    Database has camel cased ids, form had hyphenated.

--- post_billet.php
+++ post_billet.php
@@ -17,25 +17,25 @@
                 <h1>Create your billet post:</h1>
                 <form action="./scripts/CreatePost.php" method="POST">
                     <p>In-AFSC</p>
-                    <input type="text" id="In-AFSC" name="In-AFSC" placeholder="Enter AFSC of personnel you would like to receive.">
+                    <input type="text" id="InAFSC" name="InAFSC" placeholder="Enter AFSC of personnel you would like to receive.">
                     <p>In-Rank</p>
-                    <input type="text" id="In-Rank" name="In-Rank" placeholder="Enter Rank of personnel you would like to receive.">
+                    <input type="text" id="InRank" name="InRank" placeholder="Enter Rank of personnel you would like to receive.">
                     <p>In-SEI</p>
-                    <input type="text" id="In-SEI" name="In-SEI" placeholder="Enter SEI of personnel you would like to receive. (Optional)">
+                    <input type="text" id="InSEI" name="InSEI" placeholder="Enter SEI of personnel you would like to receive. (Optional)">
                     <p>In-SkillLvl</p>
-                    <input type="text" id="In-Skill Level" name="In-Skill Level" placeholder="Enter Skill Level of personnel you would like to receive. (Optional)">
+                    <input type="text" id="InSkillLevel" name="InSkillLevel" placeholder="Enter Skill Level of personnel you would like to receive. (Optional)">
                     <p>Out-AFSC</p>
-                    <input type="text" id="Out-AFSC" name="Out-AFSC" placeholder="Enter AFSC of billet you are willing to trade.">
+                    <input type="text" id="OutAFSC" name="OutAFSC" placeholder="Enter AFSC of billet you are willing to trade.">
                     <p>Out-Rank</p>
-                    <input type="text" id="Out-Rank" name="Out-Rank" placeholder="Enter Rank of billet you are willing to trade."><br>
+                    <input type="text" id="OutRank" name="OutRank" placeholder="Enter Rank of billet you are willing to trade."><br>
                     <p>Out-SEI</p>
-                    <input type="text" id="Out-SEI" name="Out-SEI" placeholder="Enter SEI of billet you are willing to trade. (Optional)">
+                    <input type="text" id="OutSEI" name="OutSEI" placeholder="Enter SEI of billet you are willing to trade. (Optional)">
 					<p>Out-SkillLvl</p>
-                    <input type="text" id="Out-Skill Level" name="Out-Skill Level" placeholder="Enter Skill Level of billet you are willing to trade. (Optional)">
+                    <input type="text" id="OutSkillLevel" name="OutSkillLevel" placeholder="Enter Skill Level of billet you are willing to trade. (Optional)">
 					<p>Position#</p>
-                    <input type="text" id="Position Number" name="Position Number" placeholder="Enter Position Number of billet. (Optional)">
+                    <input type="text" id="PositionNumber" name="PositionNumber" placeholder="Enter Position Number of billet. (Optional)">
 					<p>Description</p>
-                    <input type="text" id="description" name="description" placeholder="Enter a description of the duties for the billet you are posting.">
+                    <input type="text" id="Description" name="Description" placeholder="Enter a description of the duties for the billet you are posting.">
 					<input type="submit" id="submit" value="Create Billet Post">                                  
                 </form>
             </div>

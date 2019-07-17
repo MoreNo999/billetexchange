9728f2396f7a521808645bb79bb0c6b7d14a03eb
Moreno <jansen.moreno999@gmail.com>
2019-07-17 16:20:35 -0500

    css update

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,18 +1,18 @@
 <html>
 <head>
 </head>
-<link rel="stylesheet" type="text/css" href="./css/footer.css?ver=11">
+<link rel="stylesheet" type="text/css" href="./css/footer.css?ver=12">
 <body>
 <div class="footer">
 	<table>
 	  <tr>
 		<th><a href="home.php" title="Home Page">Home</a></th>
 		<th><a href="view_all.php" title="Browse">Browse</a></th>
+		<th>© MurkSec 2019</th>
 		<th><a href="post_billet.php" title="New Billet">Post New Billet</a></th>
 		<th><a href="my_account.php" title="My Account">My Account</a></th>
 	  </tr>
 	</table>
-		<h55> © MurkSec 2019 </h55>
 </div>
 </body>
 </html>

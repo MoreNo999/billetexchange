6cd9a09def261d7fc4170466eac90df4bff8e7ec
Moreno <jansen.moreno999@gmail.com>
2019-07-17 15:15:50 -0500

    css changes for footer

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,17 +1,18 @@
 <html>
 <head>
 </head>
-<link rel="stylesheet" type="text/css" href="./css/footer.css?ver=2">
+<link rel="stylesheet" type="text/css" href="./css/footer.css?ver=3">
 <body>
 <div class="footer">
-<table>
-  <tr>
-    <th><a href="home.php" title="Home Page">Home</a></th>
-    <th><a href="view_all.php" title="Browse">Browse</a></th>
-    <th><a href="post_billet.php" title="New Billet">Post New Billet</a></th>
-    <th><a href="my_account.php" title="My Account">My Account</a></th>
-  </tr>
-</table>
-    <h1> © MurkSec 2019 </h1>
+	<table>
+	  <tr>
+		<th><a href="home.php" title="Home Page">Home</a></th>
+		<th><a href="view_all.php" title="Browse">Browse</a></th>
+		<th><a href="post_billet.php" title="New Billet">Post New Billet</a></th>
+		<th><a href="my_account.php" title="My Account">My Account</a></th>
+	  </tr>
+	</table>
+		<h1> © MurkSec 2019 </h1>
+</div>
 </body>
 </html>

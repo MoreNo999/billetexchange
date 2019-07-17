55a07ad61add28693f4cb451ffc7dede7e5ce7a5
Tim Carlisle <gwa2100@gmail.com>
2019-07-17 09:43:52 -0500

    Pagination Fixed
    It was not working before.  Changed Array storage logic to array_push.

--- scripts/functions.php
+++ scripts/functions.php
@@ -12,9 +12,10 @@ function GetBilletPosts($con, $userid, $number = 5, $first = 0, $status = 1){
             $returnData = array();
             $iterator = 0;
             while ($stmt->fetch() and $iterator < $number + $first){
-                if ($iterator > $first){
-                    $returnData[$iterator++] = array( "ID"=>$ID, "OwnerID"=>$OwnerID, "OutAFSC"=>$OutAFSC, "OutRank"=>$OutRank, "OutSEI"=>$OutSEI, "OutSkillLevel"=>$OutSkillLevel, "InAFSC"=>$InAFSC, "InRank"=>$InRank, "InSEI"=>$InSEI, "InSkillLevel"=>$InSkillLevel, "PositionNumber"=>$PositionNumber, "Description"=>$Description, "DatePosted"=>$DatePosted, "Views"=>$Views, "Clicks"=>$Clicks, "Status"=>$Status);
+                if ($iterator >= $first){
+                    array_push($returnData, array( "ID"=>$ID, "OwnerID"=>$OwnerID, "OutAFSC"=>$OutAFSC, "OutRank"=>$OutRank, "OutSEI"=>$OutSEI, "OutSkillLevel"=>$OutSkillLevel, "InAFSC"=>$InAFSC, "InRank"=>$InRank, "InSEI"=>$InSEI, "InSkillLevel"=>$InSkillLevel, "PositionNumber"=>$PositionNumber, "Description"=>$Description, "DatePosted"=>$DatePosted, "Views"=>$Views, "Clicks"=>$Clicks, "Status"=>$Status));
                 }
+                ++$iterator;
             }
             // Account exists, now we verify the password.
             // Note: remember to use password_hash in your registration file to store the hashed passwords.

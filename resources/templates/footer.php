f25f4a422e1120f1cf23752c371d398e51480f24
Moreno <jansen.moreno999@gmail.com>
2019-07-17 13:34:12 -0500

    Fixed and added features to footer.php

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,5 +1,58 @@
-<div id="footer">
-    <b> Copyright MurkSec 2019 </b>
-</div>
+<html>
+<head>
+<style>
+
+h1{
+	text-align: center;	
+    padding: 10px;
+    color: #C6C7C8;
+
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  text-align: center;
+  padding: 10px;
+
+}
+
+a:link {
+  color: white;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:visited {
+  color: #04AFF9;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:hover {
+  color: #C6C7C8;
+  background-color: transparent;
+  text-decoration: underline;
+}
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
+</head>
+<body>
+
+<table>
+  <tr>
+    <th><a href="home.php" title="Home Page">Home</a></th>
+    <th><a href="view_all.php" title="Browse">Browse</a></th>
+    <th><a href="post_billet.php" title="New Billet">Post New Billet</a></th>
+    <th><a href="my_account.php" title="My Account">My Account</a></th>
+  </tr>
+</table>
+    <h1> © MurkSec 2019 </h3>
 </body>
 </html>

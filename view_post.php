671a4e4ecac0371c0a1f5fa715220100ffef6e43
Tim Carlisle <gwa2100@gmail.com>
2019-07-18 16:54:42 -0500

    Ready for php coding

---
+++ view_post.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    require_once("scripts/functions.php");
+    // load up your config file
+    require_once("config.php");
+     
+    require_once(TEMPLATES_PATH . "/header.php");
+
+    if (!isset($_SESSION['id'])){
+        header('Location: index.php');
+    }
+?>
+<link rel="stylesheet" type="text/css" href="/css/background.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<div id="container">
+    <div id="content">
+        <div class="w3-container w3-blue w3-mobile">
+            <h2>Browse All</h2>
+        </div>
+        <?php //FUNCTION HERE ?>
+    </div>
+    <?php
+        require_once(TEMPLATES_PATH . "/rightPanel.php");
+    ?>
+</div>
+<?php
+    require_once(TEMPLATES_PATH . "/footer.php");
+?>

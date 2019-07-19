293e7a71e37a92a0219500cf6ed57f24de20e454
Moreno <jansen.moreno999@gmail.com>
2019-07-19 16:09:38 -0500

    made the footer sticky

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 </head>
-<link rel="stylesheet" type="text/css" href="./css/footer.css?ver=1.4">
+<link rel="stylesheet" type="text/css" href="./css/footer.css?ver=1.5">
 <body>
 <div class="footer">
 	<table>

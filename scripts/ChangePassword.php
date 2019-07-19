54a056833fc214c39bf0c39328d9a6b4fa0a1b12
Tim Carlisle <gwa2100@gmail.com>
2019-07-19 13:03:09 -0500

    Created ChangePassword.php to handle changepassword formdata.

---
+++ scripts/ChangePassword.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+require_once("functions.php");
+
+//We are connected now, so lets check for post data
+if (!isset($_POST['oldPassword'], $_POST['newPassword'])) {
+    die ('Improper Form');
+}
+//Call update function
+if (ChangePassword($_POST)){
+    //Handle Success
+    $_SESSION['errorMessage'] = 'Account Updated!';
+    header('Location: ../index.php');
+}
+else{
+    //Handle Failure
+    $_SESSION['errorMessage'] = 'Account Update Failed!';
+    header('Location: ../index.php');
+}
+?>

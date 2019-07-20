d0fffb86d1b87143652c24127677fdb32aab12ae
josh <josh.pontes99@gmail.com>
2019-07-20 16:38:05 -0700

    making pages responsive

--- my_account.php
+++ my_account.php
@@ -11,30 +11,88 @@
     }
 	$data = GetSessionUserProfileData();
 ?>
-<link rel="stylesheet" type="text/css" href="../../css/home.css?ver=3">
+<link rel="stylesheet/less" type="text/css" href="../../css/my_account.less">
+<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js" ></script>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <div id="container">
     <div id="content">
         <div class="w3-container w3-blue w3-mobile">
             <h2>My Account</h2>
         </div>
-		<div style="width:20%; margin-left: 40%; margin-top: 3%; background-color: grey; text-align: center;" class="w3-mobile w3-round w3-modal-content w3-animate-zoom">                  
-            <label class="w3-text-black"><b>Username:<br></b></label>
-            <div class="w3-text-white"><b><?php echo $data['UserName'];?></b></div>
-			<label class="w3-text-black"><br><b>Rank:<br></b></label>
-            <div class="w3-text-white"><b><?php echo $data['Rank'];?></b></div>
-			<label class="w3-text-black"><br><b>Name:<br></b></label>
-            <div class="w3-text-white"><b><?php echo $data['FirstName'];?></b><b> </b><b><?php echo $data['LastName'];?></b></div>
-			<label class="w3-text-black"><br><b>Email:<br></b></label>
-            <div class="w3-text-white"><b><?php echo $data['Email'];?></b></div>
-			<label class="w3-text-black"><br><b>Phone Number:<br></b></label>
-            <div class="w3-text-white"><b><?php echo $data['PhoneNumber'];?></b></div>
-			<label class="w3-text-black"><br><b>Unit:<br></b></label>
-            <div class="w3-text-white"><b><?php echo $data['Unit'];?></b></div>
-			<label class="w3-text-black"><br><b>PAS Code (If Applicable):<br></b></label>
-            <div class="w3-text-white"><b><?php echo $data['PASCode'];?></b><br><br><br></div>
-			<div class="w3-button w3-blue w3-mobile" style="border-spacing: 30px"> <a href="edit_profile.php" title="Edit Profile">Edit Profile</a><br></div>
-			<div class="w3-button w3-blue w3-mobile" style="border-spacing: 30px"> <a href="change_password.php" title="Change Password">Change Password</a></div>
+		<div class="w3-animate-zoom card">
+            <table class="my_table">
+                <tr>
+                    <th>
+                        <b>Username:</b>
+                    </th> 
+                </tr>
+                <tr>
+                    <th>
+                        <?php echo $data['UserName'];?>
+                    </th>
+                </tr>
+                <tr>
+                    <th> <b>Rank:</b></th>
+                </tr>
+                <tr>
+                    <th>
+                        <?php echo $data['Rank'];?>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <b>Name:</b>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <?php echo $data['FirstName'];?> <?php echo $data['LastName'];?>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <b>Email:</b>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <?php echo $data['Email'];?>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <b>Phone Number:</b>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <?php echo $data['PhoneNumber'];?>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <b>Unit:</b>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <?php echo $data['Unit'];?>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <b>PAS Code (If Applicable):</b>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <?php echo $data['PASCode'];?>
+                    </th>
+                </tr>
+            </table>                  
+            
+			 <a href="edit_profile.php" title="Edit Profile"><button>Edit Profile</button></a>
+			 <a href="change_password.php" title="Change Password"><button>Change Password</button></a>
 		</div>
 		
     <?php

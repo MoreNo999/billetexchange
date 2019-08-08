3786c2319a786561598ab9bc2d938f7bcdc0773e
Danielle <medanielle@gmail.com>
2019-08-08 14:27:20 -0500

    updated support.php
    added Guide/FAQ/Contact Us to this page

--- support.php
+++ support.php
@@ -19,7 +19,29 @@
             <h2>Recent Posts</h2>
         </div>
         <div class="w-3 container w3-mobile">
-        <h3 style="color:white;"> Send an email to gwa2100@gmail.com </h3>
+        <h3 style="color:white;"> Guide </h3>
+
+        <h3 style="color:white;">
+        This site was created to allow Air Force leaders to publish billet swaps they are willing to make in their units. This will allow for a quicker and more dynamic ability to manage manpower at the unit level. </br></br>
+
+        After creating an account and logging in users will be able to: Browse all billets, Post a swap criteria, View matches. </br></br>
+
+        The Dashboard page allows users to view or delete any post they have made. </br></br>
+
+        The Browse page has three options All, Recent, or Matches. The Recent view will show all post created on the site in the last week. The Matches view will check the user's post and return any matches from other user's posts. </br></br>
+
+        The Post page is where users will submit their manning swaps.</br></br>
+
+        The My Account page is where users can edit their profile or change their password. </br></br>
+        </h3>
+
+        <h3 style="color:white;"> FAQ </h3>
+        <h3 style="color:white;">What format should the AFSC be?</br></br>
+
+        AFSC should be in the '1B4X1' format. This skill level is entered seperately later on the Post page based on what swap the user is hoping to make. </br></br> </h3>
+
+        <h3 style="color:white;"> Contact Us </h3>
+        <h3 style="color:white;"> Send an email to gwa2100@gmail.com</h3>
         </div>
     </div>
     <?php

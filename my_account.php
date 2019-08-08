717559a16968950b320be26bed6419881277d0f8
Moreno <jansen.moreno999@gmail.com>
2019-08-08 14:54:27 -0500

    Updated Responsive Look
    Added padding to CSS

--- my_account.php
+++ my_account.php
@@ -11,7 +11,7 @@
     }
 	$data = GetSessionUserProfileData();
 ?>
-<link rel="stylesheet/less" type="text/css" href="../../css/my_account.less?ver=1.1">
+<link rel="stylesheet/less" type="text/css" href="../../css/my_account.less?ver=1.2">
 <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js" ></script>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <div id="container">
@@ -19,7 +19,7 @@
         <div class="w3-container w3-blue w3-mobile">
             <h2>My Account</h2>
         </div>
-        <br><div class="box">
+        <div class="box">
             <div class="w3-animate-zoom card">
             <table class="my_table" align="center">
                 <tr>

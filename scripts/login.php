2a68f3e0f0e10df65c7dcd1fb30501b3cfb2a6da
Timothy Carlisle <gwa2100@gmail.com>
2019-08-08 13:59:19 -0500

    Fixed indent

--- scripts/login.php
+++ scripts/login.php
@@ -3,8 +3,8 @@ session_start();
 require_once("functions.php");
 require_once("../config.php");
 
-    $conMan = new SQLConnectionManager();
-    $con = $conMan->StartConnection();
+$conMan = new SQLConnectionManager();
+$con = $conMan->StartConnection();
 
 //We are connected now, so lets check for post data
 if (!isset($_POST['user'], $_POST['pass'])) {

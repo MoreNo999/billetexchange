081f2a6ffa3ca91bc35bfcbc8b87e835b16bcbc7
Moreno <jansen.moreno999@gmail.com>
2019-08-09 11:10:38 -0500

    Added MAJCOM
    Drop to MAJCOM with other box in case it is needed

--- edit_profile.php
+++ edit_profile.php
@@ -25,6 +25,7 @@
   justify-content: center;
   margin: 30px;
   text-align: center;
+  padding: 20px;
 }
 .myform{
   background-color: grey;
@@ -32,7 +33,18 @@
   border-radius: 8px;
   
 }
-</style>        
+</style>  
+<script type="text/javascript">
+function CheckMAJCOM(val){
+ var element=document.getElementById('otherMAJCOM');
+ if(val=='Other')
+   element.style.display='block';
+ else  
+   element.style.display='none';
+}
+
+</script>
+      
         <div class="box">
             <div class='myform w3-animate-zoom'> 
             <form action="./scripts/UpdateProfile.php" method="POST">                 
@@ -59,7 +71,22 @@
                 <input class="w3-input w3-border w3-light-grey" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" title="Format: 123-456-7890" value="<?php echo $data['PhoneNumber'];?>" id="phone" name="phone">
                 <label class="w3-text-black"><br><b>Unit:<br></b></label>
                 <input class="w3-input w3-border w3-light-grey" value="<?php echo $data['Unit'];?>" id="unit" name="unit">
-                <label class="w3-text-black"><br><b>PAS Code (If Applicable):<br></b></label>
+				<label class="w3-text-black"><br><b>MAJCOM:<br></b></label>
+				<select class="w3-select w3-light-grey w3-border" style="text-align: center" id="Majcom" name="Majcom" onchange="CheckMAJCOM(this.value);">
+								<option value="<?php echo $data['Majcom'];?>" selected><?php echo $data['Majcom'];?></option>
+								<option value="ACC">ACC</option>
+								<option value="AETC">AETC</option>
+								<option value="AFGSC">AFGSC</option>
+								<option value="AFMC">AFMC</option>
+								<option value="AFSPC">AFSPC</option>
+								<option value="AMC">AMC</option>
+								<option value="AFSOC">AFSOC</option>
+								<option value="PACAF">PACAF</option>
+								<option value="USAFE">USAFE</option>
+								<option value="Other">Other</option>
+                </select>
+				<input placeholder="Please Enter MAJCOM:" title="Please Enter MAJCOM:" class="w3-input w3-border w3-light-grey" type="text" name="otherMAJCOM" id="otherMAJCOM" style="display:none;"/>
+                <label class="w3-text-black"><br><br><b>PAS Code (If Applicable):<br></b></label>
                 <input class="w3-input w3-border w3-light-grey" value="<?php echo $data['PASCode'];?>" id="PASCode" name="PASCode"><br><br>
                 <input class="w3-button w3-blue" type="submit" id="submit" value="Submit Changes">
             </form>
